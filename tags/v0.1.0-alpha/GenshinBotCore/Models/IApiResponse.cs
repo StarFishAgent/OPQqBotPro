99a1108af4cfe3d5cb06dd03d9ca837de8510759
StarFish <2813035180@qq.com>
2023-05-08 17:59:52 +0800

    1

--- tags/v0.1.0-alpha/GenshinBotCore/Models/IApiResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GenshinBotCore.Models
-{
-    public interface IApiResponse<T>
-    {
-        bool IsSuccess { get; }
-        Type ApiDataType { get; }
-        T? Payload { get; }
-    }
-}
